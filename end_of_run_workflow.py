97bf02f2ed4313720bcc033a3dfad1686d425488
gbischof <bisc8233@gmail.com>
2025-04-11 16:32:26 -0400

    add the scan_id to slack message

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -5,6 +5,9 @@ from xrf_hdf5_exporter import xrf_hdf5_exporter
 from logscan import logscan
 from prefect.blocks.notifications import SlackWebhook
 
+from tiled.client import from_profile
+CATALOG_NAME = "srx"
+
 
 @task
 def log_completion():
@@ -16,14 +19,20 @@ def log_completion():
 def end_of_run_workflow(stop_doc):
     try:
         uid = stop_doc["run_start"]
+
+        # Get the scan_id
+        tiled_client = from_profile("nsls2")[CATALOG_NAME]
+        tiled_client_raw = tiled_client["raw"]
+        scan_id = tiled_client_raw[uid].start['scan_id']
+
         # data_validation(uid, return_state=True)
         xanes_exporter(uid)
         xrf_hdf5_exporter(uid)
         logscan(uid)
         log_completion()
         slack_webhook_block = SlackWebhook.load("mon-prefect")
-        slack_webhook_block.notify(f"Export successful run_start: {uid}")
+        slack_webhook_block.notify(f"Export successful\n run_start: {uid}\n scan_id: {scan_id}")
     except Exception as e:
         slack_webhook_block = SlackWebhook.load("mon-prefect")
-        slack_webhook_block.notify(f"<@U01RUN2HDPB> Export failed for run_start: {uid}")
+        slack_webhook_block.notify(f"Export failed\n run_start: {uid}\n scan_id: {scan_id}")
         raise
