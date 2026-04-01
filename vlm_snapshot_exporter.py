2788fcc59ff86cda8c6fbbe73d75241c6f5a0b22
Jun Aishima <jaishima@bnl.gov>
2026-04-01 14:22:05 -0400

    pre-commit lint fixes

--- vlm_snapshot_exporter.py
+++ vlm_snapshot_exporter.py
@@ -1,38 +1,34 @@
 from prefect import flow, task, get_run_logger
-import time as ttime
 import numpy as np
 import os
-from skimage import io
-from PIL import Image, ImageDraw, ImageFont
-import matplotlib.pyplot as plt
+from PIL import Image
 from data_validation import get_run
 
 
 @flow(log_prints=True)
 def vlm_image_exporter(ref, api_key=None, dry_run=False):
     logger = get_run_logger()
-    logger.info('')
+    logger.info("")
 
-    scan_id = get_run(uid, api_key=api_key).start['scan_id']
-    logger.info(f'Looking for snapshots in scan {scan_id}.')
+    scan_id = get_run(uid, api_key=api_key).start["scan_id"]
+    logger.info(f"Looking for snapshots in scan {scan_id}.")
 
-    export_vlm_image(scan_id,
-                     overlay=True,
-                     raw_image=True,
-                     api_key=api_key,
-                     dry_run=dry_run)
-    logger.info(f'Finished exporting any snapshots in scan {scan_id}.')
+    export_vlm_image(
+        scan_id, overlay=True, raw_image=True, api_key=api_key, dry_run=dry_run
+    )
+    logger.info(f"Finished exporting any snapshots in scan {scan_id}.")
 
 
 @task
-def export_vlm_image(scan_id,
-                     wd=None,
-                     overlay=True,
-                     raw_image=True,
-                     autoscale=True,
-                     api_key=None,
-                     dry_run=False,
-                     ):
+def export_vlm_image(
+    scan_id,
+    wd=None,
+    overlay=True,
+    raw_image=True,
+    autoscale=True,
+    api_key=None,
+    dry_run=False,
+):
     logger = get_run_logger()
 
     # Pseudocode
@@ -49,28 +45,28 @@ def export_vlm_image(scan_id,
 
     # Initial checks
     # Does scan exist
-    scan_id = int(scan_id)    
+    scan_id = int(scan_id)
     h = get_run(scan_id, api_key=api_key)
 
     # VLM image data acquired?
-    if 'camera_snapshot' not in h:
-        warn_str = f'No VLM images found for scan {scan_id}.'
+    if "camera_snapshot" not in h:
+        warn_str = f"No VLM images found for scan {scan_id}."
         logger.info(warn_str)
         return
-    
+
     if wd is None:
-        proposal_id = h.start['proposal']['proposal_id']
-        cycle = h.start['cycle']
-        wd = f'/nsls2/data/srx/proposals/{cycle}/pass-{proposal_id}/'
-    
+        proposal_id = h.start["proposal"]["proposal_id"]
+        cycle = h.start["cycle"]
+        wd = f"/nsls2/data/srx/proposals/{cycle}/pass-{proposal_id}/"
+
     # Create sub-folder
-    wd = f'{wd}vlm_snapshots/'
+    wd = f"{wd}vlm_snapshots/"
     os.makedirs(wd, exist_ok=True)
-    
-    images = h['camera_snapshot']['data']['nano_vlm_image'][:, 0].astype(np.float64)
+
+    images = h["camera_snapshot"]["data"]["nano_vlm_image"][:, 0].astype(np.float64)
 
     # logger.info('VLM images found; writing images to folder.')
-    for image, title in zip(images, ['before', 'after']):
+    for image, title in zip(images, ["before", "after"]):
         # Normalize
         image -= image.mean() - 2 * image.std()
         image /= image.mean() + 2 * image.std()
@@ -79,7 +75,7 @@ def export_vlm_image(scan_id,
 
         # Return raw png
         image = Image.fromArray((image * 65535).astype(np.uint16))
-        filename = os.path.join(wd, f'scan{scan_id}_VLM_image_{title}.png')
+        filename = os.path.join(wd, f"scan{scan_id}_VLM_image_{title}.png")
         if dry_run:
             logger.info(f"Dry run: Not saving image to {filename}")
         else:
