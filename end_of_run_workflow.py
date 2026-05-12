d935d9df033c52980a912ac5a44e820faa5511ac
Jun Aishima <jaishima@bnl.gov>
2026-05-12 17:20:32 -0400

    fix bad url in link

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -77,7 +77,7 @@ def slack(func):
             flow_run = FlowRunContext.get().flow_run
             # Add link to flow-run for the message to mon-prefect-im.
             program_message = (
-                f":bangbang: {CATALOG_NAME} flow-run failed. <https://{PREFECT_UI_URL.value()}/flow-runs/"
+                f":bangbang: {CATALOG_NAME} flow-run failed. <{PREFECT_UI_URL.value()}/flow-runs/"
                 + f"flow-run/{flow_run.id}|the flow run link> (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```{tb[-1]}```"
             )
             mon_prefect_im.notify(program_message)
